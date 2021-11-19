82c525e9d9b8acf4f17508069d93ec6af3683305
George Duckett <georgeduckett@gmail.com>
2021-11-19 12:31:12 +0000

    Autocomplete following an opening square bracket

--- src/Anaplan/AnaplanMetaData.ts
+++ src/Anaplan/AnaplanMetaData.ts
@@ -118,7 +118,7 @@ export class AnaplanMetaData {
                         lineItem[1].name,
                         this.quoteIfNeeded(lineItem[1].name),
                         monaco.languages.CompletionItemKind.Constant,
-                        [' ', ']', '+', '-', '*', '/'],
+                        [' ', '[', ']', '+', '-', '*', '/'],
                         EntityType[lineItem[1].entityType],
                         undefined));
                 }
